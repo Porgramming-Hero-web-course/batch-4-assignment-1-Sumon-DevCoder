7747d0f675d380905ea67f17ec6bea1878a92f47
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-11-09 13:33:16 +0600

    problem 4 solved

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const removeDuplicates = (nums: number[]): number[] => {
 // console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
 
 // Problem 3:
-const countWordOccurrences = (sentence: string, word: string) => {
+const countWordOccurrences = (sentence: string, word: string): number => {
   const escapedWord = word.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
   const regex = new RegExp(escapedWord, "g");
@@ -28,3 +28,38 @@ const countWordOccurrences = (sentence: string, word: string) => {
 };
 
 // console.log(countWordOccurrences("I love typescript", "typescript"));
+
+// Problem 4:
+type TCircle = {
+  shape: "circle";
+  radius: number;
+};
+
+type TRectangle = {
+  shape: "rectangle";
+  width: number;
+  height: number;
+};
+
+type TShape = TCircle | TRectangle;
+
+const calculateShapeArea = (shape: TShape) => {
+  if (shape.shape === "rectangle") {
+    return shape.height * shape.width;
+  } else if (shape.shape === "circle") {
+    return Math.PI * shape.radius * shape.radius;
+  }
+};
+
+const rectangleArea = calculateShapeArea({
+  shape: "rectangle",
+  width: 4,
+  height: 6,
+});
+
+console.log(rectangleArea);
+
+// Problem 5:
+const getProperty = (obj, name) => {};
+
+// Problem 6:
