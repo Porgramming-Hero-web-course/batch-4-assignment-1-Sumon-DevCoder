59da75bdffe92f60072caf63a095ae3c21329cb1
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-11-09 09:00:52 +0600

    problem 1 and 2 solved

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// Problem 1:
+const sumArray = (nums: number[]): number => {
+  const result: number = nums.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+
+  return result;
+};
+
+// console.log(sumArray([1, 2, 3, 4, 5]));
+
+// Problem 2:
+const removeDuplicates = (nums: number[]): number[] => {
+  const result: number[] = [...new Set(nums)];
+  return result;
+};
+
+// console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+
+// Problem 3:
